b749ace5f5e155d2ed0bead3a032819655006ae1
Joao Chaves <jvmchaves@gmail.com>
2026-05-23 22:32:17 -0400

    encontrando os triangulos

---
+++ programas/pg2.py
@@ -0,0 +1,18 @@
+"""Escreva um programa que peça à pessoa usuária três números que representam os lados de um triângulo. O programa deve informar se os valores podem ser utilizados para formar um triângulo e, caso afirmativo, se ele é equilátero, isósceles ou escaleno. Tenha em mente algumas dicas:
+
+Três lados formam um triângulo quando a soma de quaisquer dois lados for maior que o terceiro;
+Triângulo Equilátero: três lados iguais;
+Triângulo Isósceles: quaisquer dois lados iguais;
+Triângulo Escaleno: três lados diferentes."""
+
+lado1 = float(input('Digite o primeiro lado do triângulo: '))
+lado2 = float(input('Digite o segundo lado do triângulo: '))
+lado3 = float(input('Digite o terceiro lado do triângulo: '))
+
+if lado1 + lado2 > lado3 and lado1 + lado3 > lado2 and lado2 + lado3 > lado1:
+    if lado1 == lado2 == lado3:
+        print('O triângulo é equilátero.')
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print('O triângulo é isósceles.')
+    else:
+        print('O triângulo é escaleno.')
